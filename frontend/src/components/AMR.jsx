6004d008fa34a7b74d6ef03efe15ed4de2179874
David Schaeper <dwschaeper12@gmail.com>
2026-07-13 20:23:46 -0400

    add gene by location plot

--- frontend/src/components/AMR.jsx
+++ frontend/src/components/AMR.jsx
@@ -1,11 +1,12 @@
 import { useEffect, useState } from "react"
 import Plot from "react-plotly.js"
 
-async function load_data(organism) {
-    console.log("Loading data for organism:", organism)  // Debugging statement to check the value of organism
+async function load_data(organism, location) {
+    console.log("Loading data for organism:", organism, ' and location:', location)  // Debugging statement
     // set with parameter
     const params = new URLSearchParams({
-        organism: organism
+        organism: organism,
+        location: location
     })
     const url = `http://localhost:8000/amr?${params}`
 
@@ -20,15 +21,16 @@ function AMR() {
     // initialize state variables
     const [data, setData] = useState(null)
     const [selectedOrganism, setSelectedOrganism] = useState('All Organisms')
+    const [selectedLocation, setSelectedLocation] = useState("Indianapolis,Indiana")
 
     // fetch data
     useEffect(() => {
-        const fetchData = async (selectedOrganism) => {
-            const result = await load_data(selectedOrganism)
+        const fetchData = async (selectedOrganism, selectedLocation) => {
+            const result = await load_data(selectedOrganism, selectedLocation)
             setData(result)
         }
-        fetchData(selectedOrganism)
-    }, [selectedOrganism])
+        fetchData(selectedOrganism, selectedLocation)
+    }, [selectedOrganism, selectedLocation])
 
     return (
         <>
@@ -36,53 +38,100 @@ function AMR() {
             <h3>Antibiotic Resistance Overview</h3>
         </div>
 
-        {/*Create organism selection*/}
-        <div style={{padding: '20px', paddingLeft: '50px'}}>
-            <form>
-                <label>Choose an organism:
-                    <select value={selectedOrganism} onChange={(e) => setSelectedOrganism(e.target.value)}>
-                        <option value='All Organisms'>All Organisms</option>
-                        {data && data.organisms.map((organism) => (
-                            <option key={organism} value={organism}>{organism}</option>
-                        ))}
-                    </select>
-                </label>
-            </form>
-        </div>
+        <div className='plot-container'>
+            <div className='plot-grid'>
+                
+                {/*Create organism selection*/}
+                <div>
+                    <form>
+                        <label>Choose an organism:
+                            <select value={selectedOrganism} onChange={(e) => setSelectedOrganism(e.target.value)}>
+                                <option value='All Organisms'>All Organisms</option>
+                                {data && data.organisms.map((organism) => (
+                                    <option key={organism} value={organism}>{organism}</option>
+                                ))}
+                            </select>
+                        </label>
+                    </form>
 
-        <div>
-            {data && (
-                <Plot
-                    data={[
-                        {
-                            x: Object.keys(data.drug_class_counts),
-                            y: Object.values(data.drug_class_counts),
-                            type: 'bar'
-                        }
-                    ]}
-                    layout={{
-                        title: {
-                            text: `Number of Samples by Drug Class Resistance<br>(${selectedOrganism})`
-                        },
-                        xaxis: {
-                            title: {
-                                text: 'Drug Class'
-                            }
-                        },
-                        yaxis: {
-                            title: {
-                                text: 'Number of Samples'
-                            }
-                        },
-                        margin: {
-                            t: 35,
-                            b: 100
-                        }
-                    }}
-                />
-                )}
-        </div>
-        </>
+                    {data && (
+                        <Plot
+                            data={[
+                                {
+                                    x: Object.keys(data.drug_class_counts),
+                                    y: Object.values(data.drug_class_counts),
+                                    type: 'bar'
+                                }
+                            ]}
+                            layout={{
+                                title: {
+                                    text: `Number of Samples by Drug Class Resistance<br>(${selectedOrganism})`
+                                },
+                                xaxis: {
+                                    title: {
+                                        text: 'Drug Class'
+                                    }
+                                },
+                                yaxis: {
+                                    title: {
+                                        text: 'Number of Samples'
+                                    }
+                                },
+                                margin: {
+                                    t: 35,
+                                    b: 100
+                                }
+                            }}
+                        />
+                    )}
+                </div>
+
+                {/*Create location selection*/}
+                <div>
+                    <form>
+                        <label>Choose a Location:
+                            <select value={selectedLocation} onChange={(e) => setSelectedLocation(e.target.value)}>
+                                {data && data.locations.map((location) => (
+                                    <option key={location} value={location}>{location}</option>
+                                ))}
+                            </select>
+                        </label>
+                    </form>
+
+                    {data && (
+                        <Plot
+                            data={[
+                                {
+                                    x: Object.keys(data.location_gene_counts),
+                                    y: Object.values(data.location_gene_counts),
+                                    type: 'bar'
+                                }
+                            ]}
+                            layout={{
+                                title: {
+                                    text: `Number of Samples by Gene in ${selectedLocation}`
+                                },
+                                xaxis: {
+                                    title: {
+                                        text: 'Gene'
+                                    }
+                                },
+                                yaxis: {
+                                    title: {
+                                        text: 'Number of Samples'
+                                    }
+                                },
+                                margin: {
+                                    t: 35,
+                                    b: 100
+                                }
+                            }}
+                        />
+                    )}
+                </div>
+            </div>
+        </div> 
+    </>
     )
 }
 
